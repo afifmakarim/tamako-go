0031fb89c290aacf4d8a694a271391b294c87d4c
afifmakarim <afifmakarim88@gmail.com>
2020-03-02 22:58:36 +0700

    new

--- main.go
+++ main.go
@@ -9,51 +9,57 @@ import (
 	"github.com/line/line-bot-sdk-go/linebot"
 )
 
+var bot *linebot.Client
+
 func main() {
-	// os.Setenv("CHANNEL_SECRET", "1be4fbd94dd56738610507b0c20de840")
-	// os.Setenv("CHANNEL_TOKEN", "SnCXDhD8ZOMoDJv4+JdRIT/XVjkOIhdY2/v/YECnPH1hJx+ttwK0pqXsXyfemvWPadHCnqZNEIRWx/j6VzxGw4zBM1ahAJIIJyfODtGCTS2YipB0r+1NF1efb/NvGGuCFc6Kut6x6oa4J4zoX0jTyQdB04t89/1O/w1cDnyilFU=")
-
-	bot, err := linebot.New(
-		os.Getenv("CHANNEL_SECRET"),
-		os.Getenv("CHANNEL_TOKEN"),
-	)
+	// load configuration
+	var err error
+	bot, err = linebot.New(os.Getenv("CHANNEL_SECRET"), os.Getenv("CHANNEL_TOKEN"))
 	if err != nil {
-		log.Fatal(err)
+		log.Println("Bot Initial Error:", err)
 	}
 
-	// Setup HTTP Server for receiving requests from LINE platform
-	http.HandleFunc("/webhook", func(w http.ResponseWriter, req *http.Request) {
-		events, err := bot.ParseRequest(req)
-		if err != nil {
-			if err == linebot.ErrInvalidSignature {
-				w.WriteHeader(400)
-				//fmt.Fprint(w, "Invalid Signature")
-			} else {
-				w.WriteHeader(500)
-				fmt.Fprint(w, "ok")
-			}
-			return
+	http.HandleFunc("/", hello)
+	http.HandleFunc("/callback", callBack)
+	log.Fatal(http.ListenAndServe(":8000", nil))
+
+}
+
+func hello(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("hello"))
+	w.WriteHeader(200)
+}
+
+func callBack(w http.ResponseWriter, req *http.Request) {
+	events, err := bot.ParseRequest(req)
+
+	if err != nil {
+		if err == linebot.ErrInvalidSignature {
+			w.WriteHeader(400)
+		} else {
+			w.WriteHeader(500)
 		}
-		for _, event := range events {
-			if event.Type == linebot.EventTypeMessage {
-				switch message := event.Message.(type) {
-				case *linebot.TextMessage:
-					if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(message.Text)).Do(); err != nil {
-						log.Print(err)
-					}
-				case *linebot.StickerMessage:
-					replyMessage := fmt.Sprintf(
-						"sticker id is %s, stickerResourceType is %s", message.StickerID, message.StickerResourceType)
-					if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(replyMessage)).Do(); err != nil {
-						log.Print(err)
-					}
-				}
-			}
-		}
-	})
-	// This is just sample code.
-	// For actual use, you must support HTTPS by using `ListenAndServeTLS`, a reverse proxy or something else.
-	if err := http.ListenAndServe(":"+os.Getenv("PORT"), nil); err != nil {
-		log.Fatal(err)
+		return
 	}
+
+	for _, event := range events {
+		if event.Type == linebot.EventTypeMessage {
+			switch message := event.Message.(type) {
+			case *linebot.TextMessage:
+
+				// extract message details
+				fmt.Printf("reply channel:%s, msg:%s, user id:%s\n", event.ReplyToken, message.Text,
+					event.Source.UserID)
+
+				// reply
+				if _, err = bot.ReplyMessage(event.ReplyToken,
+					linebot.NewTextMessage(message.Text)).Do(); err != nil {
+					log.Print(err)
+				}
+
+			}
+		}
+
+	}
+	return
 }
