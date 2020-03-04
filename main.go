1ca68551a64d00dbf62eaecba14579e5ecf1eb9b
afifmakarim <afifmakarim88@gmail.com>
2020-03-04 22:41:27 +0700

    new

--- main.go
+++ main.go
@@ -46,7 +46,7 @@ func callBack(w http.ResponseWriter, req *http.Request) {
 	for _, event := range events {
 		if event.Type == linebot.EventTypeMessage {
 			// switch message := event.Message.(type) {
-			switch message := event.Message.(type) {
+			switch message.Text {
 			case "help":
 
 				// extract message details
