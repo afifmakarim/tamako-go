587fd10679d09cc526e15d126f9903f48cb2ba79
afifmakarim <afifmakarim88@gmail.com>
2020-03-07 22:17:27 +0700

    new

--- main.go
+++ main.go
@@ -245,8 +245,8 @@ func (app *TamakoBot) handleText(message *linebot.TextMessage, replyToken string
 				return err
 			}
 		case "write":
-			sentence := string(keyword[5:])
-			rawEncoded := Rawurlencode(string(sentence[1:]))
+			sentence := string(keyword[6:])
+			rawEncoded := Rawurlencode(sentence)
 			var imageUrl string
 
 			if sentence == "" || message.Text == "!write" {
