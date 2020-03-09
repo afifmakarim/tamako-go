42193d242823181eeeecc12755d782b44dea46f0
afifmakarim <afifmakarim88@gmail.com>
2020-03-09 18:54:11 +0700

    games

--- main.go
+++ main.go
@@ -722,7 +722,97 @@ func (app *TamakoBot) gameMessage(message string, replyToken string) error {
 	gameApi := Request("https://www.giantbomb.com/api/search/?api_key=a0bede1760f86f2f59ff3ac477c953fed643ea0b&resources=game&query="+queryGame+"&format=json&limit=5", "lashaparesha api script")
 	json.Unmarshal([]byte(gameApi), &gameList)
 
-	jsonString := FlexGameJson(gameList.Results[0].Name)
+	jsonString := `{
+		"type": "bubble",
+		"header": {
+		  "type": "box",
+		  "layout": "vertical",
+		  "contents": [
+			{
+			  "type": "box",
+			  "layout": "horizontal",
+			  "contents": [
+				{
+				  "type": "image",
+				  "url": "https://giantbomb1.cbsistatic.com/uploads/scale_small/9/97089/2915098-main.jpg",
+				  "size": "full",
+				  "aspectMode": "cover",
+				  "aspectRatio": "4:3",
+				  "gravity": "center"
+				}
+			  ]
+			}
+		  ],
+		  "paddingAll": "0px"
+		},
+		"body": {
+		  "type": "box",
+		  "layout": "vertical",
+		  "contents": [
+			{
+			  "type": "box",
+			  "layout": "vertical",
+			  "contents": [
+				{
+				  "type": "box",
+				  "layout": "vertical",
+				  "contents": [
+					{
+					  "type": "text",
+					  "contents": [],
+					  "size": "xl",
+					  "wrap": true,
+					  "text": "Tales of Berseria",
+					  "color": "#ffffff",
+					  "weight": "bold"
+					},
+					{
+					  "type": "text",
+					  "text": "Release Date : 2016-08-18",
+					  "color": "#ffffffcc",
+					  "size": "sm"
+					},
+					{
+					  "type": "text",
+					  "text": "Platform : PC, PS4, Nintendo Switch",
+					  "color": "#ffffffcc",
+					  "size": "sm"
+					}
+				  ],
+				  "spacing": "sm"
+				},
+				{
+				  "type": "box",
+				  "layout": "vertical",
+				  "contents": [
+					{
+					  "type": "box",
+					  "layout": "vertical",
+					  "contents": [
+						{
+						  "type": "text",
+						  "contents": [],
+						  "size": "sm",
+						  "wrap": true,
+						  "margin": "lg",
+						  "color": "#ffffffde",
+						  "text": "The sixteenth mainline entry in the long-running Tales action-RPG series, following the exploits of a pirate named Velvet."
+						}
+					  ]
+					}
+				  ],
+				  "paddingAll": "13px",
+				  "backgroundColor": "#ffffff1A",
+				  "cornerRadius": "2px",
+				  "margin": "xl"
+				}
+			  ]
+			}
+		  ],
+		  "paddingAll": "20px",
+		  "backgroundColor": "#464F69"
+		}
+	  }`
 	contents, err := linebot.UnmarshalFlexMessageJSON([]byte(jsonString))
 	if err != nil {
 		return err
