f6646cdb93fefce0006cb9b39581c35ce118fede
afifmakarim <afifmakarim88@gmail.com>
2020-03-09 19:12:54 +0700

    games

--- main.go
+++ main.go
@@ -466,61 +466,157 @@ func (app *TamakoBot) handleText(message *linebot.TextMessage, replyToken string
 					{
 					  "type": "box",
 					  "layout": "vertical",
-					  "contents": [
-						{
+					  {
+						"type": "bubble",
+						"hero": {
+						  "type": "image",
+						  "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/01_1_cafe.png",
+						  "size": "full",
+						  "aspectRatio": "20:13",
+						  "aspectMode": "cover",
+						  "action": {
+							"type": "uri",
+							"uri": "http://linecorp.com/"
+						  }
+						},
+						"body": {
 						  "type": "box",
 						  "layout": "vertical",
 						  "contents": [
 							{
 							  "type": "text",
-							  "contents": [],
-							  "size": "xl",
-							  "wrap": true,
-							  "text": "Cony Residence",
-							  "color": "#ffffff",
-							  "weight": "bold"
+							  "text": "Brown Cafe",
+							  "weight": "bold",
+							  "size": "xl"
 							},
 							{
-							  "type": "text",
-							  "text": "3 Bedrooms, ¥35,000",
-							  "color": "#ffffffcc",
-							  "size": "sm"
-							}
-						  ],
-						  "spacing": "sm"
-						},
-						{
-						  "type": "box",
-						  "layout": "vertical",
-						  "contents": [
+							  "type": "box",
+							  "layout": "baseline",
+							  "margin": "md",
+							  "contents": [
+								{
+								  "type": "icon",
+								  "size": "sm",
+								  "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gold_star_28.png"
+								},
+								{
+								  "type": "icon",
+								  "size": "sm",
+								  "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gold_star_28.png"
+								},
+								{
+								  "type": "icon",
+								  "size": "sm",
+								  "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gold_star_28.png"
+								},
+								{
+								  "type": "icon",
+								  "size": "sm",
+								  "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gold_star_28.png"
+								},
+								{
+								  "type": "icon",
+								  "size": "sm",
+								  "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gray_star_28.png"
+								},
+								{
+								  "type": "text",
+								  "text": "4.0",
+								  "size": "sm",
+								  "color": "#999999",
+								  "margin": "md",
+								  "flex": 0
+								}
+							  ]
+							},
 							{
 							  "type": "box",
 							  "layout": "vertical",
+							  "margin": "lg",
+							  "spacing": "sm",
 							  "contents": [
 								{
-								  "type": "text",
-								  "contents": [],
-								  "size": "sm",
-								  "wrap": true,
-								  "margin": "lg",
-								  "color": "#ffffffde",
-								  "text": "Private Pool, Delivery box, Floor heating, Private Cinema"
+								  "type": "box",
+								  "layout": "baseline",
+								  "spacing": "sm",
+								  "contents": [
+									{
+									  "type": "text",
+									  "text": "Place",
+									  "color": "#aaaaaa",
+									  "size": "sm",
+									  "flex": 1
+									},
+									{
+									  "type": "text",
+									  "text": "Miraina Tower, 4-1-6 Shinjuku, Tokyo",
+									  "wrap": true,
+									  "color": "#666666",
+									  "size": "sm",
+									  "flex": 5
+									}
+								  ]
+								},
+								{
+								  "type": "box",
+								  "layout": "baseline",
+								  "spacing": "sm",
+								  "contents": [
+									{
+									  "type": "text",
+									  "text": "Time",
+									  "color": "#aaaaaa",
+									  "size": "sm",
+									  "flex": 1
+									},
+									{
+									  "type": "text",
+									  "text": "10:00 - 23:00",
+									  "wrap": true,
+									  "color": "#666666",
+									  "size": "sm",
+									  "flex": 5
+									}
+								  ]
 								}
 							  ]
 							}
 						  ],
-						  "paddingAll": "13px",
-						  "backgroundColor": "#ffffff1A",
-						  "cornerRadius": "2px",
-						  "margin": "xl"
+						  "backgroundColor": "#11223333"
+						},
+						"footer": {
+						  "type": "box",
+						  "layout": "vertical",
+						  "spacing": "sm",
+						  "contents": [
+							{
+							  "type": "button",
+							  "style": "link",
+							  "height": "sm",
+							  "action": {
+								"type": "uri",
+								"label": "CALL",
+								"uri": "https://linecorp.com"
+							  }
+							},
+							{
+							  "type": "button",
+							  "style": "link",
+							  "height": "sm",
+							  "action": {
+								"type": "uri",
+								"label": "WEBSITE",
+								"uri": "https://linecorp.com"
+							  }
+							},
+							{
+							  "type": "spacer",
+							  "size": "sm"
+							}
+						  ],
+						  "flex": 0
 						}
-					  ]
-					}
-				  ],
-				  "paddingAll": "20px",
-				  "backgroundColor": "#464F69"
-				}
-			  }`
+					  }`
 			contents, err := linebot.UnmarshalFlexMessageJSON([]byte(jsonString))
 			if err != nil {
 				return err
