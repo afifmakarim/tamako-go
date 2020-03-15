5822f8368133f94f3a466a71f88cc0f25bb40af8
afifmakarim <afifmakarim88@gmail.com>
2020-03-15 16:07:57 +0700

    new

--- main.go
+++ main.go
@@ -945,7 +945,7 @@ func (app *TamakoBot) mangaMessage(message string, replyToken string) error {
 	json.Unmarshal([]byte(get_manga), &getManga)
 	//get_genre_endpoint := getManga.Data[0].Relationships.Links.Self
 
-	return app.replyText(replyToken, getManga.Data[0].Attributes.CanonicalTitle+getManga.Data[0].Relationships.Genres.Link.Self)
+	return app.replyText(replyToken, getManga.Data[0].Attributes.CanonicalTitle+getManga.Data[0].Relationships.Genres.Links.Self)
 }
 
 func (app *TamakoBot) replyText(replyToken, text string) error {
